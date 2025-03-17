ad9cbca403d6d4fa7c46ab631d555871085f9cfd
Assouli Imad <122219125+Imad1assouli@users.noreply.github.com>
2025-03-17 09:19:51 +0000

    Update user.effects.ts

--- src/app/features/user/store/user.effects.ts
+++ src/app/features/user/store/user.effects.ts
@@ -28,4 +28,43 @@ export class UserEffects{
   );
 });
 }
+/*
+ // Effet pour obtenir un utilisateur par ID
+  getUserById$ = createEffect(() => {
+    return this.action$.pipe(
+      ofType(UserActions.getUserById),
+      switchMap(action =>
+        this.userService.getUserById(action.id).pipe(
+          map(user => UserActions.loadUserByIdSuccess({ user })),
+          catchError(error => of(UserActions.loadUserByIdFailure({ error: error.message })))
+        )
+      )
+    );
+  });
 
+  // Effet pour mettre à jour un utilisateur
+  updateUser$ = createEffect(() => {
+    return this.action$.pipe(
+      ofType(UserActions.updateUser),
+      switchMap(action =>
+        this.userService.updateUser(action.user).pipe(
+          map(user => UserActions.updateUserSuccess({ user })),
+          catchError(error => of(UserActions.updateUserFailure({ error: error.message })))
+        )
+      )
+    );
+  });
+
+  // Effet pour supprimer un utilisateur
+  deleteUser$ = createEffect(() => {
+    return this.action$.pipe(
+      ofType(UserActions.deleteUser),
+      switchMap(action =>
+        this.userService.deleteUser(action.id).pipe(
+          map(() => UserActions.deleteUserSuccess({ id: action.id })),
+          catchError(error => of(UserActions.deleteUserFailure({ error: error.message })))
+        )
+      )
+    );
+  });
+*/
